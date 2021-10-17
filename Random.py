ec711cc6724cf8a5dfc7981e5627c97632cd16de
Vadim <Vagor3000@yandex.ru>
2021-10-17 16:45:55 +0300

    Угадайка, добавленно указание на количество попыток и добавлена возможность указать диапазон предельных значенией

--- Random.py
+++ Random.py
@@ -1,16 +1,18 @@
 # Данный модулб является минипроектом
 import random
 
-n = random.randint(1, 100)
 print()
 print('Добро пожаловать в числовую угадайку!')
+k = int(input('Введите максимальное значение для нашей игры: '))
+print('Значение загадано!')
+n = random.randint(1, k)
 
 
-def is_valid(z):
+def is_valid(z, i):
     if not z.isdigit():
         return False
     else:
-        if 0 < int(z) < 101:
+        if 0 < int(z) < i + 1:
             return True
         else:
             return False
@@ -22,15 +24,17 @@ cnt = 0
 
 while not flag:
     m = input('Введите предполагаемое значение: ')
-    if not is_valid(m):
+    if not is_valid(m, k):
         print('А может быть все-таки введем число от 1 до 100?')
     else:
         cnt += 1
         m = int(m)
         if m < n:
             print('Ваше число меньше загаданного, попробуйте еще разок')
+            print()
         elif m > n:
             print('Ваше число больше загаданного, попробуйте еще разок')
+            print()
         else:
             print()
             print('Вы угадали за', cnt, 'попыток поздравляем!')
